92915ca2700a0dc2687435ea8e462d2c1794b0a2
Kate White <white.kate.e@gmail.com>
2016-07-16 20:18:24 -0600

    Fixed issues I was seeing after deplying to Heroku

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -21,7 +21,6 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
   app.import("bower_components/imagesloaded/imagesloaded.pkgd.min.js");
   app.import("bower_components/scrollreveal/dist/scrollreveal.min.js");
-  app.import("bower_components/foundation-sites/dist/foundation.min.js");
 
   return app.toTree();
 };
