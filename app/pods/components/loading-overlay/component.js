0663112b72948fdc18a59b0ce62edfb06fa9bd03
Kate White <white.kate.e@gmail.com>
2016-07-18 09:53:39 -0600

    Added Montserrat from google fonts as a back up to gibson to see if that works on IE

--- app/pods/components/loading-overlay/component.js
+++ app/pods/components/loading-overlay/component.js
@@ -1,8 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['loading-overlay'],
-	didUpdateAttrs() {
-		this._super(...arguments);
-	}
+	classNames: ['loading-overlay']
 });
