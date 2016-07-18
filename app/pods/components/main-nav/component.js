f7671f2e44254feeb94b4185568fea4f5e9c3355
Kate White <white.kate.e@gmail.com>
2016-07-18 16:03:23 -0600

    Modified font sizes on mobile so everything doesn't look so magnified

--- app/pods/components/main-nav/component.js
+++ app/pods/components/main-nav/component.js
@@ -2,22 +2,5 @@ import Ember from 'ember';
 import HandleOnLoad from 'justkatewhite/mixins/handle-onload';
 
 export default Ember.Component.extend(HandleOnLoad, {
-	classNames: ['main-nav'],
-	actions: {
-		toggleAnimation: function() {
-			//Add the animate-css class "animated" to all items with the to-animate class
-			var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-			var $elem = $('.to-animate');
-			$elem = this.reset($elem);
-			console.log('Adding "animated" class');
-	        $elem.addClass('animated');
-		}
-	},
-
-	reset: function($elem) {
-		$elem.before($elem.clone(true));
-		var $newElem = $elem.prev();
-		$elem.remove();
-		return $newElem;
-	}
+	classNames: ['main-nav']
 });
