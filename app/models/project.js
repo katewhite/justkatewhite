2a76a6f7761b94740a7697145404f7d97f965040
Kate White <white.kate.e@gmail.com>
2016-07-19 16:52:19 -0600

    Responsive images and svgs for project banner logos

--- app/models/project.js
+++ app/models/project.js
@@ -11,5 +11,7 @@ export default Model.extend({
 	thumbnailImageSrcset: attr(),
 	thumbnailImageSizes: attr(),
 	bannerImage: attr(),
+	bannerImageSrcset: attr(),
+	bannerImageSizes: attr(),
 	contents: hasMany('content')
 });
